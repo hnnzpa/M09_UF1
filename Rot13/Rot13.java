488d60cf6739d66f6ac8a8e1672371159430212f
Hannita <hnnzpa@gmail.com>
2025-09-24 21:25:31 +0200

    Finalitzat l'exercici

--- Rot13/Rot13.java
+++ Rot13/Rot13.java
@@ -2,7 +2,12 @@ public class Rot13{
 
     static char[] lletres = "aàbcçdéèefghiíìjklmnñóòopqrstúùuvwxyz".toCharArray();
     public static void main(String[] args) {
-        System.out.println(xifraRot13("Hola, que tal?"));
+        String cadena = "Hola, que tal?";
+        String cadenaXifrada = xifraRot13(cadena); 
+        System.out.println("El text xifrat: " + cadenaXifrada);
+        String desxifrat = desxifraRot13(cadenaXifrada); 
+        System.out.println("Cadena desxifrada a partir del text xifrat: " + desxifrat);
+        
     }
 
     public static String xifraRot13(String cadena){
@@ -49,7 +54,42 @@ public class Rot13{
     }
     
 
-    public static String desxifraRot13(String cadena){ return ""; }
+    public static String desxifraRot13(String cadena){ 
+        char[] cadenaC = cadena.toCharArray(); 
+        String textDesxifrat = ""; 
+
+        for (int i = 0; i <= cadena.length()-1; i++){
+            char lletra = cadenaC[i];
+            boolean gran = false;
+            if (Character.isLetter(lletra)){
+
+                if (Character.isUpperCase(lletra)){
+                    gran = true; 
+                }
+
+                lletra = Character.toLowerCase(lletra);
+                int pos = posicio(lletra);
+
+                if (pos == -1){ // no està a l'array, l'afegim tal qual
+                    textDesxifrat += lletra; 
+                    continue; 
+                }
+
+                // desplaçar 13 enrere (amb volta si cal)
+                pos -= 13;
+                if (pos < 0) pos += lletres.length;
+
+                // mirar si era maj i afegir la lletra corresponent
+                if (gran){
+                    textDesxifrat += Character.toUpperCase(lletraXPosicio(pos)); 
+                }else{
+                    textDesxifrat += lletraXPosicio(pos);
+                }
+            }else {
+                textDesxifrat += lletra; 
+            }
+        }
+        return textDesxifrat; }
 
     public static int posicio(char cT){
         for (int i = 0; i <= lletres.length-1; i++){
